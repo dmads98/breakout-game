e95449b73968a3d2305ece80f136b56506efa306
Dhanush Madabusi <dm322@duke.edu>
2019-01-19 14:47:13 -0500

    Example program

---
+++ src/example/Example.java
@@ -0,0 +1,50 @@
+package example;
+
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+
+public class Example extends Application{
+
+    @Override
+    public void start (Stage primaryStage) throws Exception{
+        Line line = new Line();
+
+        line.setStartX(50.0);
+        line.setStartY(50.0);
+        line.setEndX(100.0);
+        line.setEndY(100.0);
+
+        Text text = new Text();
+        text.setFont(new Font(40.0));
+        text.setX(60.0);
+        text.setY(150.0);
+        text.setText("Welcome to Breakout");
+
+        Group root = new Group();
+
+        ObservableList list = root.getChildren();
+        list.add(line);
+        list.add(text);
+
+        Scene scene = new Scene(root, 600, 300);
+
+        scene.setFill(Color.PINK);
+
+        primaryStage.setTitle("Sample App");
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+    public static void main (String args[]){
+        launch(args);
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module game_dm322 {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.media;
+
+    exports example;
+}
